0e2efdebca97abe10284a3a687c80db62b170f79
Artem-Hvedinich <artem33771@gmail.com>
2023-04-25 21:04:00 +0300

    first

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,16 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import NeedView from '../views/NeedView.vue'
 import ThankView from '../views/ThankView.vue'
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomeView
+        },
         {
             path: '/need',
             name: 'need',
---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div style="display: grid; gap: 20px">
+    <h2>Хотим молиться за тебя!</h2>
+    <p>Радуйтесь с радующимися, плачьте с плачущими.<br/>Римлянам 12:15 </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeView"
+}
+</script>
+
+<style scoped>
+
+</style>
