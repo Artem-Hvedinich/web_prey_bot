47ba09ea6dc03da65e60549e806a36d5c2d35e74
Artem-Hvedinich <artem33771@gmail.com>
2023-04-26 14:12:50 +0300

    first

--- src/App.vue
+++ src/App.vue
@@ -10,11 +10,10 @@ const tg = window.Telegram.WebApp
 
 <template>
   <header>
-    <p>Это телеграм бот церкви Чистое сердце</p>
-    <div>
+    <nav>
       <router-link to="/need">Нужда</router-link>
       <router-link to="/thank">Благодарность</router-link>
-    </div>
+    </nav>
   </header>
   <main>
     <RouterView/>
@@ -31,7 +30,6 @@ header {
   justify-content: center;
   flex-direction: column;
   align-items: center;
-  gap: 10px;
   border-bottom: 1px solid red;
 }
 
@@ -48,60 +46,30 @@ footer {
   background: red;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
 nav {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 50px;
   width: 100%;
+  gap: 30px;
   font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
 }
 
-nav a.router-link-exact-active {
+nav a.router-link-active {
   color: orange;
+  text-decoration: none;
+  border-bottom: 1px solid orange;
+  padding-bottom: 2px;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+nav a:hover {
+  color: blue;
+  text-decoration: none;
 }
 
 nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid yellow;
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+  color: red;
+  text-decoration: none;
 }
 </style>
